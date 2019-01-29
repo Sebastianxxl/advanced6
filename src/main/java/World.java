cf22350c0722dbc779dda2fa18fdb6e44624d4f4
Grupa 2 Java <seinho_>
2019-01-29 17:59:22 +0200

    import

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,17 +1,16 @@
-import javax.naming.Name;
 import java.sql.*;
 
 public class World {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost/world?username=root&password=root&useSSL=false"
+                    "jdbc:mysql://localhost/world?user=root&password=root&useSSL=false&allowPublicKeyRetrieval=true"
             );
             Statement statement = connection.createStatement();
 
             ResultSet result = statement.executeQuery("SELECT * FROM city");
 
-            while(result.next()) {
+            while (result.next()) {
                 System.out.println(result.getString("Name"));
             }
         } catch (SQLException e) {
